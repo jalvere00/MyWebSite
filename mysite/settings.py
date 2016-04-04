69e2ea8e2202ddd2e063f99518dd1ba274a377e7
Jameel Alverez <jameelalverez@yahoo.com>
2016-04-03 21:44:36 -0400

    Made nessecery changes to the view to allow static files to be read

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,17 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'jameeldj',
+#         'USER':'jameelpg',
+#         'PASSWORD':'sdr001',
+#         'HOST':'localhost',
+#         'PORT':'',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jameeldj',
-        'USER':'jameelpg',
-        'PASSWORD':'sdr001',
-        'HOST':'localhost',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -124,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
